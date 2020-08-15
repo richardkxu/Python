196cbe5a75fd96ddb7af7474048004ae8c61c4ab
richardkxu <richardkxu@gmail.com>
2020-08-14 20:44:11 -0700

    add bias term to logistic reg, optimize code

--- machine_learning/logistic_regression.py
+++ machine_learning/logistic_regression.py
@@ -1,13 +1,3 @@
-#!/usr/bin/python
-
-# Logistic Regression from scratch
-
-# In[62]:
-
-# In[63]:
-
-# importing all the required libraries
-
 """
 Implementing logistic regression for classification problem
 Helpful resources:
@@ -18,59 +8,77 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import datasets
 
-# get_ipython().run_line_magic('matplotlib', 'inline')
-
-
-# In[67]:
-
-# sigmoid function or logistic function is used as a hypothesis function in
-# classification problems
-
 
 def sigmoid_function(z):
     return 1 / (1 + np.exp(-z))
 
 
-def cost_function(h, y):
-    return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+def cost_function(a, y):
+    """
+    hinge loss
+    """
+    return (-y * np.log(a) - (1 - y) * np.log(1 - a)).mean()
 
 
-def log_likelihood(X, Y, weights):
-    scores = np.dot(X, weights)
-    return np.sum(Y * scores - np.log(1 + np.exp(scores)))
+def forward(X, w, b):
+    """
+    forward pass
+        If b is None, then no bias term.
+    """
+    z = np.dot(X, w)
+    if b is not None:
+        z += b
+    a = sigmoid_function(z)
+    return a
+
+
+def backprop(alpha, w, b, a, y):
+    dz = a - y
+    dw = np.dot(X.T, dz) / y.size
+    db = None
+    w = w - alpha * dw  # updating the weights
+    if b is not None:
+        db = np.sum(dz) / y.size
+        b = b - alpha * db
+    return w, b
 
 
 # here alpha is the learning rate, X is the feature matrix,y is the target matrix
-def logistic_reg(alpha, X, y, max_iterations=70000):
-    theta = np.zeros(X.shape[1])
+def logistic_reg(alpha, X, y, bias=True, max_iterations=70000):
+    """
+    logistic regression trained with gradient descent
+    """
+    w = np.zeros(X.shape[1])  # without bias para
+    b = None
+    if bias:
+        b = np.zeros(1)
 
     for iterations in range(max_iterations):
-        z = np.dot(X, theta)
-        h = sigmoid_function(z)
-        gradient = np.dot(X.T, h - y) / y.size
-        theta = theta - alpha * gradient  # updating the weights
-        z = np.dot(X, theta)
-        h = sigmoid_function(z)
-        J = cost_function(h, y)
+        a = forward(X, w, b)
+        w, b = backprop(alpha, w, b, a, y)
+        a = forward(X, w, b)
+        J = cost_function(a, y)
         if iterations % 100 == 0:
-            print(f"loss: {J} \t")  # printing the loss after every 100 iterations
-    return theta
+            print(f"loss: {J}")  # printing the loss after every 100 iterations
+    return w, b
 
-
-# In[68]:
 
 if __name__ == "__main__":
     iris = datasets.load_iris()
     X = iris.data[:, :2]
     y = (iris.target != 0) * 1
+    print("shape of X: {}".format(X.shape))
+    print("shape of y: {}".format(y.shape))
 
     alpha = 0.1
-    theta = logistic_reg(alpha, X, y, max_iterations=70000)
-    print("theta: ", theta)  # printing the theta i.e our weights vector
+    w, b = logistic_reg(alpha, X, y, bias=True, max_iterations=70000)
+    print("w: ", w)  # printing the w i.e our weights vector
+    if b is not None:
+        print("b: ", b)
 
     def predict_prob(X):
         return sigmoid_function(
-            np.dot(X, theta)
+            np.dot(X, w)
         )  # predicting the value of probability from the logistic regression algorithm
 
     plt.figure(figsize=(10, 6))
@@ -80,7 +88,7 @@ if __name__ == "__main__":
     (x2_min, x2_max) = (X[:, 1].min(), X[:, 1].max())
     (xx1, xx2) = np.meshgrid(np.linspace(x1_min, x1_max), np.linspace(x2_min, x2_max))
     grid = np.c_[xx1.ravel(), xx2.ravel()]
-    probs = predict_prob(grid).reshape(xx1.shape)
+    probs = forward(grid, w, b).reshape(xx1.shape)
     plt.contour(xx1, xx2, probs, [0.5], linewidths=1, colors="black")
 
     plt.legend()
